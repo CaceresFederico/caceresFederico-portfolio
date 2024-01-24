633336490526831289d8c2e4577e4ebefec575bf
Federico <Caceresfederico@hotmail.com>
2024-01-24 05:34:31 -0300

    Nav change to fixed, Landing text edited, Html y css code enhanced, Enhanced responsive design in sm resolution, Icons added, Resources tinyfied, Favicon added

--- js/main.js
+++ js/main.js
@@ -4,19 +4,9 @@ window.addEventListener('scroll', () => {
         let windowHeight = window.innerHeight;
         let revealTop = items[i].getBoundingClientRect().top;
         let revealPoint = 100;
-        revealTop < windowHeight - revealPoint ? items[i].classList.add('active') : items[i].classList.remove('active');
+        revealTop < windowHeight - revealPoint ?
+            items[i].classList.add('active') :
+            items[i].classList.remove('active');
     }
 })
 
-// const navButtons = document.querySelectorAll('.nav-item')
-// const navItemChangeColor = (e) => {
-//     const selectedBtn = e
-//     return console.log(e)
-// }
-
-// const buttons = document.querySelector('.nav-item')
-// buttons.addEventListener('click', (e)=>{
-//     console.log(e)
-// })
-
-// console.log(buttons)
